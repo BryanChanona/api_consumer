46a6b35498aed21b8ada25a1814a48f5b73652d9
BryanChanona <233299@ids.upchiapas.edu.mx>
2025-03-20 16:08:34 -0600

    Agregando la conexión a la base de datos.

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"github.com/BryanChanona/api_consumer/src/helpers"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	 helpers.ConnMySQL()
+	 r:= gin.Default()
+
+	 r.Run(":8081")
+}
